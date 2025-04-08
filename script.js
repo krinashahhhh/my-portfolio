9061e1f32d06198fbc906f31c475e9fedde8f08f
Krina Shah <krinas393@gmail.com>
2025-04-08 17:46:54 -0400

    intermediate portfolio upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+// function toggleMenu() {
+//   const menu = document.querySelector(".menu-links");
+//   const icon = document.querySelector(".hamburger-icon");
+//   menu.classList.toggle("open");
+//   icon.classList.toggle("open");
+// }
 
+
+
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("open");
+}
